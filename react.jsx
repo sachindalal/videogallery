b515695d3eaec25e5ec65fa38c79ccf63f3e1296
Sachin <badmaashsachin@gmail.com>
2021-03-29 18:17:03 +0530

    Update react.jsx
    passed values from board to square on click

--- react.jsx
+++ react.jsx
@@ -1,14 +1,8 @@
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state= {
-      value: null,
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
